da78a2247962902efbbf60d2dfbf29c7463cde63
qingmo <qmswf.27@gmail.com>
2024-04-26 22:29:27 +0800

    upload
    Signed-off-by: qingmo <qmswf.27@gmail.com>

---
+++ src/weather.cpp
@@ -0,0 +1,81 @@
+#include "weather.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "ArduinoUZlib.h"
+const char *qweather_ca =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIGEzCCA/ugAwIBAgIQfVtRJrR2uhHbdBYLvFMNpzANBgkqhkiG9w0BAQwFADCB\n"
+    "iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n"
+    "cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n"
+    "BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTgx\n"
+    "MTAyMDAwMDAwWhcNMzAxMjMxMjM1OTU5WjCBjzELMAkGA1UEBhMCR0IxGzAZBgNV\n"
+    "BAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UEBxMHU2FsZm9yZDEYMBYGA1UE\n"
+    "ChMPU2VjdGlnbyBMaW1pdGVkMTcwNQYDVQQDEy5TZWN0aWdvIFJTQSBEb21haW4g\n"
+    "VmFsaWRhdGlvbiBTZWN1cmUgU2VydmVyIENBMIIBIjANBgkqhkiG9w0BAQEFAAOC\n"
+    "AQ8AMIIBCgKCAQEA1nMz1tc8INAA0hdFuNY+B6I/x0HuMjDJsGz99J/LEpgPLT+N\n"
+    "TQEMgg8Xf2Iu6bhIefsWg06t1zIlk7cHv7lQP6lMw0Aq6Tn/2YHKHxYyQdqAJrkj\n"
+    "eocgHuP/IJo8lURvh3UGkEC0MpMWCRAIIz7S3YcPb11RFGoKacVPAXJpz9OTTG0E\n"
+    "oKMbgn6xmrntxZ7FN3ifmgg0+1YuWMQJDgZkW7w33PGfKGioVrCSo1yfu4iYCBsk\n"
+    "Haswha6vsC6eep3BwEIc4gLw6uBK0u+QDrTBQBbwb4VCSmT3pDCg/r8uoydajotY\n"
+    "uK3DGReEY+1vVv2Dy2A0xHS+5p3b4eTlygxfFQIDAQABo4IBbjCCAWowHwYDVR0j\n"
+    "BBgwFoAUU3m/WqorSs9UgOHYm8Cd8rIDZsswHQYDVR0OBBYEFI2MXsRUrYrhd+mb\n"
+    "+ZsF4bgBjWHhMA4GA1UdDwEB/wQEAwIBhjASBgNVHRMBAf8ECDAGAQH/AgEAMB0G\n"
+    "A1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAbBgNVHSAEFDASMAYGBFUdIAAw\n"
+    "CAYGZ4EMAQIBMFAGA1UdHwRJMEcwRaBDoEGGP2h0dHA6Ly9jcmwudXNlcnRydXN0\n"
+    "LmNvbS9VU0VSVHJ1c3RSU0FDZXJ0aWZpY2F0aW9uQXV0aG9yaXR5LmNybDB2Bggr\n"
+    "BgEFBQcBAQRqMGgwPwYIKwYBBQUHMAKGM2h0dHA6Ly9jcnQudXNlcnRydXN0LmNv\n"
+    "bS9VU0VSVHJ1c3RSU0FBZGRUcnVzdENBLmNydDAlBggrBgEFBQcwAYYZaHR0cDov\n"
+    "L29jc3AudXNlcnRydXN0LmNvbTANBgkqhkiG9w0BAQwFAAOCAgEAMr9hvQ5Iw0/H\n"
+    "ukdN+Jx4GQHcEx2Ab/zDcLRSmjEzmldS+zGea6TvVKqJjUAXaPgREHzSyrHxVYbH\n"
+    "7rM2kYb2OVG/Rr8PoLq0935JxCo2F57kaDl6r5ROVm+yezu/Coa9zcV3HAO4OLGi\n"
+    "H19+24rcRki2aArPsrW04jTkZ6k4Zgle0rj8nSg6F0AnwnJOKf0hPHzPE/uWLMUx\n"
+    "RP0T7dWbqWlod3zu4f+k+TY4CFM5ooQ0nBnzvg6s1SQ36yOoeNDT5++SR2RiOSLv\n"
+    "xvcRviKFxmZEJCaOEDKNyJOuB56DPi/Z+fVGjmO+wea03KbNIaiGCpXZLoUmGv38\n"
+    "sbZXQm2V0TP2ORQGgkE49Y9Y3IBbpNV9lXj9p5v//cWoaasm56ekBYdbqbe4oyAL\n"
+    "l6lFhd2zi+WJN44pDfwGF/Y4QA5C5BIG+3vzxhFoYt/jmPQT2BVPi7Fp2RBgvGQq\n"
+    "6jG35LWjOhSbJuMLe/0CjraZwTiXWTb2qHSihrZe68Zk6s+go/lunrotEbaGmAhY\n"
+    "LcmsJWTyXnW0OMGuf1pGg+pRyrbxmRE1a6Vqe8YAsOf4vmSyrcjC8azjUeqkk+B5\n"
+    "yOGBQMkKW+ESPMFgKuOXwIlCypTPRpgSabuY0MLTDXJLR27lk8QyKGOHQ+SwMj4K\n"
+    "00u/I5sUKUErmgQfky3xxzlIPK1aEn8=\n"
+    "-----END CERTIFICATE-----\n";
+const char *url = "https://devapi.qweather.com/v7/weather/now?location=101010100&key=****";
+String getweather()
+{
+    HTTPClient http;
+    http.begin(url, qweather_ca);
+    http.setTimeout(10000);
+    int httpCode = http.GET();
+    Serial.printf("HTTP 状态码: %d", httpCode);
+    WiFiClient *stream = http.getStreamPtr();                               // 获取HTTP响应流
+    int size = http.getSize();                                              // 获取 HTTP 响应体的大小
+    uint8_t inbuff[size];                                                   // 初始化inbuff数组
+    stream->readBytes(inbuff, size);                                        // 将http流数据写入inbuff中
+    uint8_t *outbuf = NULL;                                                 // 解压后的输出流
+    uint32_t outresult = 0;                                                 // 解压后的大小，在调用解压方法后会被赋值。
+    int result = ArduinoUZlib::decompress(inbuff, size, outbuf, outresult); // 调用解压函数
+    Serial.write(outbuf, outresult);                                        // 输出解压
+    Serial.println("响应数据\n");
+    Serial.println(outresult);
+    http.end();
+    String jsonStr = String((char *)outbuf); // 将解压后的数据转换为字符串
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, jsonStr); // 将json字符串转换为json对象
+    if (error)
+    {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return "<error>";
+    }
+    String temp = doc["now"]["temp"].as<String>();
+    String obsTime = doc["now"]["obsTime"].as<String>();
+    String windSpeed = doc["now"]["windSpeed"].as<String>();
+
+    Serial.print("\ntemp: ");
+    Serial.println(temp);
+    Serial.print("Observation Time: ");
+    Serial.println(obsTime);
+
+    String combinedData = "current temperature: " + String(temp) + "\ncurrent time: " + String(obsTime) + "\nCurrent wind speed: " + String(windSpeed);
+    ;
+    return combinedData;
+}
---
+++ src/weather.h
@@ -0,0 +1,8 @@
+#ifndef WEATHER_H
+#define WEATHER_H
+
+#include <Arduino.h>
+
+String getweather();
+
+#endif
